0b31c204a0a05839f5e10fd502b3bd89df31b97c
bala <bala.bizsoft@gmail.com>
2018-07-22 16:41:33 +0530

    Installed HammerJS

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
+  ], exports: [
+    MatButtonModule
   ],
   declarations: []
 })
